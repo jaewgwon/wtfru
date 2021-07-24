967d58deb8ebb2a78c7d094a71ff7e42e0c1ddd7
김상민 <wrt123123123@gmail.com>
2021-07-24 16:41:21 +0900

    Build network layer

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,77 @@
 import service from "./service/service";
 
-export default function session() {
-    const password = this.status(true)
+function getSession(hostData) {
+    return service ({
+        url: "api/seesion?password=true",
+        method: "get",
+        hostData
+    });
+};
 
-    service.post("api/session?password=" + {password})
+function postSession(hostData) {
+    return service ({
+        url: "api/session?password=true",
+        method: "post", 
+        hostData
+    });
+};
 
-}
+function deleteSession(hostData) {
+    return service ({
+        url: "api/session?password=true",
+        method: "delete",
+        hostData
+    });
+};
+
+function getLocation(clientData) {
+    return service ({
+        url: ("api/session" + {uid} + "/location/" + {locationId}),
+        method: "get",
+        clientData
+    });
+};
+
+function postLocation(clientData) {
+    return service ({
+        url: ("api/session/" + {uid} + "/location/" + {locationId}),
+        method: "post", 
+        clientData
+    });
+};
+
+function patchLocation(clientData) {
+    return service ({
+        url: ("api/session/" + {uid} + "/location/" + {locationId}),
+        method: "patch",
+        clientData
+    });
+};
+
+function getStatus(clientData) {
+    return service ({
+        url: ("api/session/" + {uid} + "/status"),
+        method: "get",    
+        clientData
+    });
+};
+
+function putStatus(clientData) {
+    return service ({
+        url: ("api/session/" + {uid} + "/status"),
+        method: "put",
+        clientData
+    });
+};
+
+
+export { 
+    getSession,
+    postSession,
+    deleteSession,
+    getLocation,
+    postLocation,
+    patchLocation,
+    getStatus,
+    putStatus
+};
