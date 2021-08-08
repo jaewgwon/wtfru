0d528ef3d1e6d3496c7522fda7e350cbd8c81a35
김상민 <wrt123123123@gmail.com>
2021-08-08 19:20:23 +0900

    feat: Add Vuex for store in location, stauts, session

---
+++ frontend/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import LocationStore from "./location"
+import SessionStore from "./session"
+import StatusStore from "./status"
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  moduled: {
+    location: LocationStore,
+    session: SessionStore,
+    status: StatusStore
+  }
+})
---
+++ frontend/src/store/status/status.js
@@ -0,0 +1,59 @@
+import { getStatus, putStatus } from "../../api/index"
+
+// state
+const state = {
+    uid: [],
+    status: [],
+    messgae: 'Status had been update',
+    error: ''
+}
+
+// mutations
+const mutations = {
+    GET_STATE(state, status, uid) {
+        state.status = status,
+        state.uid = uid
+    },
+    PUT_STATE(state, status, uid, message) {
+        state.status = status,
+        state.uid = uid,
+        state.message = message
+    },
+    error(state, error) {
+        return state.error = error
+    }
+}
+
+// actions
+const actions = {
+    GET_STATUS() {
+        getStatus()
+        .then(response => {
+            uid.commit('GET_STATE',response.data);
+        })
+        .catch(error => {
+            context.commit('error', error)
+        })
+    },
+    PUT_STATUS() {
+        putStatus()
+        .then(response => {
+            uid.commit('PUT_STATE',response.data);
+        })
+        .catch(error => {
+            context.commit('error', error)
+        })
+    },
+}
+
+// getters
+const getters = {
+
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
