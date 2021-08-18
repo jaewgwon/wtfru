3aa93e50792b6b3a0822e394c168e7758d601612
김상민 <wrt123123123@gmail.com>
2021-08-18 16:58:10 +0900

    feat: Add MainComponent Button & Layout
    b-card안에 들어갈 이미지는 프로젝트 페이지를 캡쳐하여 사용하기 위해 src부분은 비워두었습니다.

---
+++ frontend/src/components/button/MainButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-button">
+    <b-button class="main-button-create" size="md" pill variant="primary" href="/create">Create Room</b-button>
+    <b-button class="main-button-join" size="md" pill variant="success" href="/join">Join Room</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainButton'
+}
+</script>
+
+<style scoped>
+@media screen and (max-width:575px){
+  .main-button{
+    padding-top: 15%;
+  }
+  .main-button-join {
+    margin-left: 2%;
+    padding-left: 21px;
+    padding-right: 21px;
+  }
+}
+@media screen and (min-width:576px) {
+  .main-button{
+    padding-top: 15%;
+  }
+  .main-button-create {
+    margin-right: 13px;
+    padding: 5px 20px 5px 20px;
+    font-size: 20px;
+  }
+  .main-button-join {
+    margin-left: 13px;
+    padding: 5px 31px 5px 31px;
+    font-size: 20px;
+  }
+}
+</style>
---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-container fluid="xl">
+    <div class="header">
+      <div class="main-typography">
+        <h1><strong>Welcome to WTFRU</strong></h1><br/>
+        <h4>You can start creating sessions and locate your friends!!</h4>
+      </div>
+      <div class="main-button">
+        <MainButton/>
+      </div>
+    </div>
+    <MainGrid/>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainButton from '../components/button/MainButton.vue'
+import MainGrid from '../components/grid/MainGrid.vue'
+
+export default {
+  name: 'MainComponents',
+  components: {
+    MainButton,
+    MainGrid
+  }
+}
+</script>
+
+<style scoped>
+@media screen and (min-width:576px) {
+  .main-typography {
+    margin-top: 15%;
+  }
+}
+@media screen and (max-width: 575px) {
+  .main-typography {
+    margin-top: 35%;
+  }
+}
+
+</style>
