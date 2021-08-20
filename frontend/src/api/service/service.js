3e6b744568c6e82164484fd9b4bc7d7a0cb3285e
김상민 <wrt123123123@gmail.com>
2021-08-20 19:46:23 +0900

    style: Fix API Style
    ESLint 기반 (, ; " ' space)등을 수정하였습니다.

--- frontend/src/api/service/service.js
+++ frontend/src/api/service/service.js
@@ -1,25 +1,24 @@
-import axios from "axios";
+import axios from 'axios'
 
 const service = axios.create({
-    baseURL: "http://localhost:8080/",
+  baseURL: 'http://localhost:8080/'
 })
 
 service.interceptors.request.use(config => {
-    if (localStorage.token) {
-        config.headers['authorization'] = 'Bearer' + JSON.parse(localStorage.token)
-    }
-    return config
+  if (localStorage.token) {
+    config.headers.authorization = 'Bearer' + JSON.parse(localStorage.token)
+  }
+  return config
 }, error => {
-    return Promise.reject('Requset error', error)
+  return Promise.reject(error)
 })
 
 service.interceptors.response.use(res => {
-    if (200 <= res.status && 300 > res.status) {
-        return res.data
-    } else throw Error("HTTP ERROR", res.status)
-
+  if (res.status >= 200 && res.status < 300) {
+    return res.data
+  } else throw Error('HTTP ERROR', res.status)
 }, error => {
-    return Promise.reject('Response error', error)
+  return Promise.reject(error)
 })
 
-export default service;
+export default service
