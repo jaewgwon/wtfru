a63cedab72b90f9881bff9b6765ccbad4491e851
psymoney <sangykimiv@gmail.com>
2021-08-20 13:10:17 +0900

    chore: Fix location service for changed attribution
    LocationServcie attribute 변화에 맞추어 관련 interface 및 class
    수정하였습니다.
    Token을 통해 전달된 SessionTitle로 UID 확인하는 로직을 추가하였습니다.

--- backend/src/main/java/com/wtfru/backend/service/LocationService.java
+++ backend/src/main/java/com/wtfru/backend/service/LocationService.java
@@ -4,9 +4,9 @@ import com.wtfru.backend.dto.LocationDTO;
 import com.wtfru.backend.exception.DataIOException;
 
 public interface LocationService {
-    public LocationDTO postLocation(LocationDTO location);
+    public LocationDTO postLocation(String sessionTitle, LocationDTO location);
 
-    public LocationDTO getLocation(String sessionUid);
+    public LocationDTO getLocation(String sessionTitle);
 
     public void patchLocation(LocationDTO location) throws DataIOException.SessionNotFoundException;
 }
