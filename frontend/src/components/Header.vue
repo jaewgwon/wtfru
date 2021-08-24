9670cfbed3c2f9915f555af77ee7b1fb1a0b1d3b
김상민 <wrt123123123@gmail.com>
2021-08-24 21:01:01 +0900

    feat: Add Header components
    다국어 적용은 프로젝트 마무리 단계에서 진행하도록 하겠습니다.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <b-navbar variant="light">
+    <b-navbar-brand href="/">WTFRU</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">한국어</b-dropdown-item>
+          <b-dropdown-item href="#">Engli</b-dropdown-item>
+          <b-dropdown-item href="#">日本</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
