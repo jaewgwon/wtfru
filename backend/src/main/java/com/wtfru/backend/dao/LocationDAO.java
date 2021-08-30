1be8e9780dfabf8aa122bf813e6961818707e5c5
psymoney <sangykimiv@gmail.com>
2021-08-30 14:14:37 +0900

    Remove ResultMap annoataion and change it to Results annotation

--- backend/src/main/java/com/wtfru/backend/dao/LocationDAO.java
+++ backend/src/main/java/com/wtfru/backend/dao/LocationDAO.java
@@ -6,17 +6,15 @@ import com.wtfru.backend.dto.LocationDTO;
 
 @Repository
 public interface LocationDAO {
-    @Results(id = "locationDTO", value = {
-            @Result(property = "locationId", column = "location_id", id = true),
-            @Result(property = "sessionUid", column = "session_uid"),
-            @Result(property = "updatedDate", column = "updated_date")
-    })
-
     @Insert("insert into locations (session_uid, latitude, longitude) " +
             "values (#{sessionUid}, #{latitude}, #{longitude})")
     public boolean post(String sessionUid, Double latitude, Double longitude);
 
-    @ResultMap("locationDTO")
+    @Results({
+            @Result(property = "locationId", column = "location_id", id = true),
+            @Result(property = "sessionUid", column = "session_uid"),
+            @Result(property = "updatedDate", column = "updated_date")
+    })
     @Select("select * from locations where session_uid=#{sessionUid}")
     public LocationDTO get(String sessionUid);
 
