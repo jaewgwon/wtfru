6a1f9e90cfeaa89ce61e175a2bbf0c8fd25586eb
김상민 <wrt123123123@gmail.com>
2021-09-01 21:40:05 +0900

    feat: Add SessionJoinComponent

---
+++ frontend/src/views/Join.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="join">
+  <b-container fluid="xl">
+    <h1 class="join-session-name">Join Session</h1>
+    <div class="join-components-input">
+      <JoinInput/>
+    </div>
+  </b-container>
+</div>
+</template>
+
+<script>
+import JoinInput from '../components/input/JoinInput.vue'
+
+export default {
+  name: 'SessionJoinComponents',
+  components: {
+    JoinInput
+  }
+}
+</script>
+
+<style scoped>
+.join-session-name {
+  text-align: center;
+}
+@media screen and (min-width:576px) {
+  .join {
+    margin-top: 5vh;
+  }
+  .join-components-input {
+    margin-top: 5vh;
+  }
+}
+@media screen and (max-width: 575px) {
+  .join {
+    margin-top: 5vh;
+  }
+  .join-components-input {
+    margin-top: 5vh;
+  }
+  /* .join-components-button {
+    margin-top: 20vw;
+  } */
+}
+</style>
